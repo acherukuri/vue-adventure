2849309bf0d4a6a9a85a1aa2f644744769df748c
Anil Cherukuri <anil.cherukuri03@gmail.com>
2022-02-26 15:05:42 +0000

    Adds vue getting started repo

---
+++ vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a
+            v-bind:href="github_url"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            :href="twitter_url"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderBarLinks",
+  data() {
+    return {
+      github_url: "https://github.com/johnpapa/vue-getting-started",
+      twitter_url: "https://twitter.com/john_papa",
+    };
+  }
+}
+</script>
---
+++ vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="content-container">
+    <div class="section content-title-group">
+      <h2 class="title">Heroes</h2>
+    </div>
+    <div class="columns">
+      <div class="column is-8">
+        <div class="card edit-detail">
+          <header class="card-header">
+            <p class="card-header-title">{{hero.description}}</p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <div class="field">
+                <label class="label" for="id">id</label>
+                <label class="input" id="id" readonly>{{hero.id}}</label>
+              </div>
+              <div class="field">
+                <label class="label" for="firstName">first name</label>
+                <input class="input" id="firstName" v-model="hero.firstName"/>
+              </div>
+              <div class="field">
+                <label class="label" for="lastName">last name</label>
+                <input class="input" id="lastName" v-model="hero.lastName"/>
+              </div>
+              <div class="field">
+                <label class="label" for="description">description</label>
+                <textarea class="input" id="description" type="text" v-model="hero.description"/>
+              </div>
+              <div class="field">
+                <label class="label">cape color</label>
+                <label class="radio" for="color-red">
+                  <input type="radio" id="color-red" value="red" v-model="hero.capeColor"/>
+                  red
+                </label>
+                <label class="radio" for="color-blue">
+                  <input type="radio" id="color-blue" value="blue" v-model="hero.capeColor"/>
+                  blue
+                </label>
+                <label class="radio" for="color-green">
+                  <input type="radio" id="color-green" value="green" v-model="hero.capeColor"/>
+                  green
+                </label>
+                <div class="color-line" v-bind:style="{ 'background-color': hero.capeColor }"></div>
+              </div>
+              <div class="field">
+                <label for="power">
+                  super power
+                  <div class="select is-primary">
+                    <select id="power" v-model="hero.power" @keyup.esc="clearPower" v-bind:class="{invalid: !hero.power }">
+                      <option disabled value>Please select one</option>
+                      <option>Speed</option>
+                      <option>Flight</option>
+                      <option>Strength</option>
+                      <option>Invisibility</option>
+                    </select>
+                  </div>
+                </label>
+              </div>
+              <div class="field">
+                <label class="checkbox" for="active">
+                  active
+                  <input type="checkbox" class="is-primary" id="active" v-model="hero.active"/>
+                </label>
+              </div>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <button class="link card-footer-item cancel-button" v-on:click="clearMessage">
+              <i class="fas fa-undo"></i>
+              <span>Cancel</span>
+            </button>
+            <button class="link card-footer-item" v-on:click="saveMessage">
+              <i class="fas fa-save"></i>
+              <span>Save</span>
+            </button>
+          </footer>
+        </div>
+        <div class="notification is-info"><pre>{{ message }}</pre></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Heroes',
+  data() {
+    return {
+      hero: {
+        id: 1,
+        firstName: 'Clark',
+        lastName: 'Kent',
+        capeColor: 'blue',
+        description: 'Superman',
+        power: '',
+        active: true,
+      },
+      message: '',
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+    saveMessage() {
+      this.message = JSON.stringify(this.hero);
+    },
+    clearPower() {
+      this.hero.power = '';
+    }
+  }
+};
+</script>
